4241fd7a09e1c34b42aa14e3418717361b7c731a
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-11-13 01:25:08 +0900

    Test print build log

--- xcresulttool-example/ViewController.swift
+++ xcresulttool-example/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
-        super.viewDidLoad(
+        print("")
+        
+        super.viewDidLoad())
     }
 }
